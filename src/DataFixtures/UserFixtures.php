c52242cc9e38dae9e13940a6f0ffcef29e7212a5
Hadilou1998 <hadilissilame@gmail.com>
2024-10-02 14:16:23 +0200

    Updated project profilautop

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();
+
+        // Create users
+        $user = new User();
+        $user
+            ->setEmail('merlin@profiluser.fr')
+            ->setPassword($this->hasher->hashPassword($user, 'sorcier'))
+            ->setRoles(['ROLE_USER'])
+            ->setFirstName($faker->firstName)
+            ->setLastName($faker->lastName)
+            ->setImage($faker->imageUrl(640, 480, 'people', true));
+        $manager->persist($user);
+        $this->addReference('user_1', $user);
+
+        // Create more users
+        for ($i = 2; $i <= 10; $i++) {
+            $user = new User();
+            $user
+                ->setEmail($faker->email)
+                ->setPassword($this->hasher->hashPassword($user, 'password' . $i))
+                ->setRoles(['ROLE_USER'])
+                ->setFirstName($faker->firstName)
+                ->setLastName($faker->lastName)
+                ->setImage($faker->imageUrl(640, 480, 'people', true));
+            $manager->persist($user);
+            $this->addReference('user_' . $i, $user);
+        }
+
+        $manager->flush();
+    }
+}
