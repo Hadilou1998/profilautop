835019bde8acf153c63751dbc7de5f7dfce15a77
Hadilou1998 <hadilissilame@gmail.com>
2024-10-03 16:52:45 +0200

    Updated templates and added new routes job offer

--- src/Controller/JobOfferController.php
+++ src/Controller/JobOfferController.php
@@ -2,43 +2,72 @@
 
 namespace App\Controller;
 
+use App\Entity\JobOffer;
+use App\Repository\JobOfferRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class JobOfferController extends AbstractController
 {
+    private $em;
+    private $jobOfferRepository;
+
+    public function __construct(EntityManagerInterface $em, JobOfferRepository $jobOfferRepository)
+    {
+        $this->em = $em;
+        $this->jobOfferRepository = $jobOfferRepository;   
+    }
+
     #[Route('/job-offers', name: 'app_offers', methods: ['GET'])]
     public function list(): Response
     {
+        $jobOffers = $this->jobOfferRepository->findAll();
+
         return $this->render('job_offer/list.html.twig', [
-            'controller_name' => 'JobOfferController',
+            'jobOffers' => $jobOffers,
         ]);
     }
 
     #[Route('/job-offers/new', name: 'app_offer_new', methods: ['GET', 'POST'])]
     public function new(): Response
     {
+        // Create new job offer logic here
+        $jobOffer = new JobOffer();
+
+        // Save the job offer to the database
+        $this->em->persist($jobOffer);
+        $this->em->flush();
+
         return $this->render('job_offer/new.html.twig', [
-            'controller_name' => 'JobOfferController',
+            'jobOffer' => $jobOffer,
         ]);
     }
 
     #[Route('/job-offers/{id}', name: 'app_offer_show', methods: ['GET'])]
     public function show($id): Response
     {
+        // Retrieve job offer by ID
+        $jobOffer = $this->jobOfferRepository->find($id);
+
+        if (!$jobOffer) {
+            throw $this->createNotFoundException('Job offer not found');
+        }
+
         return $this->render('job_offer/show.html.twig', [
-            'controller_name' => 'JobOfferController',
-            'id' => $id,
+            'jobOffer' => $jobOffer,
         ]);
     }
 
     #[Route('/job-offers/{id}/edit', name: 'app_offer_edit', methods: ['GET', 'POST'])]
     public function edit($id): Response
     {
+        // Edit job offer logic here
+        return $this->redirectToRoute('app_offers');
+
         return $this->render('job_offer/edit.html.twig', [
-            'controller_name' => 'JobOfferController',
-            'id' => $id,
+            'jobOffer' => $jobOffer,
         ]);
     }
 
