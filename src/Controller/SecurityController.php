699f5f9817148619c398ab8468110ee55daa92cd
Hadilou1998 <hadilissilame@gmail.com>
2024-10-04 14:00:29 +0200

    Updated 5 files by project profilautop

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,25 +2,26 @@
 
 namespace App\Controller;
 
+use App\Form\LoginFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
-    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request, Security $security): Response
     {
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
+        if ($security->isGranted('PUBLIC_ACCESS')) {
+            return $this->redirectToRoute('app_dashboard');
+        }
+        
+        $form = $this->createForm(LoginFormType::class);
 
         return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
+            'loginForm' => $form->createView(),
         ]);
     }
 
