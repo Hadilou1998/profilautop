0ff4f214eb217f9b30b1e8bdb8aa79a1fd61e5c3
Hadilou1998 <hadilissilame@gmail.com>
2024-10-04 12:20:07 +0200

    Updated Project

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,29 +3,37 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
+use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register', methods: ['GET' ,'POST'])]
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
     {
         $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form = $this->createForm(RegistrationType::class, $user);
+        
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
+            // encode the password
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+            $user->setCreatedAt(new \DateTimeImmutable());
+            $user->setUpdatedAt(new \DateTimeImmutable());
+            $user->setRoles(['ROLE_USER']);
+            
             $em->persist($user);
             $em->flush();
 
             // Redirect to the login after successful registration
-            return $this->redirectToRoute('login');
+            return $this->redirectToRoute('app_login');
         }
+
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
