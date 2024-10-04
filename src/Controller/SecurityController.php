4aa26308f9ebac6b60834b3fa45e07958b17751c
Hadilou1998 <hadilissilame@gmail.com>
2024-10-04 10:29:25 +0200

    Updated Project

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'security_login', methods: ['GET', 'POST'])]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
